cc6254f5304bb2e3c28a8e888183f8292e2d0674
Nate Finch <nfinch2@wisc.edu>
2024-02-25 00:58:21 -0600

    update practice

--- chapter-22-hooks/starter/src/Practice3.js
+++ chapter-22-hooks/starter/src/Practice3.js
@@ -3,7 +3,7 @@ import React from "react";
 const Practice3 = () => {
   /*
    * 1. Create a value in state using hooks called catImg
-   * 2. Use useEffect to make an API call to https://aws.random.cat/meow for a random picture of a cat
+   * 2. Use useEffect to make an API call to https://cataas.com/cat for a random picture of a cat
    * 3. Add the url for the image to the page below the title
    * 4. Pass an empty array of dependencies to useEffect so it only fetches on initial page load
    */
